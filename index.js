2767c8e8216418c0dccbed93ce3b55f3d9dfb473
Ayush Mishra <ayusham001@gmail.com>
2023-08-01 23:32:20 +0530

    a

--- index.js
+++ index.js
@@ -8,29 +8,49 @@ const io = new Server(server);
 const connectedUsers = {};
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected:', socket.id);
+  console.log('a user connected:', socket.id);
 
-    socket.on('setNickname', (nickname) => {
-        connectedUsers[socket.id] = nickname;
-        io.emit('user connected', nickname);
-        io.emit('active users', Object.values(connectedUsers));
-    });
+  socket.on('setNickname', (nickname) => {
+    connectedUsers[socket.id] = { nickname, connection: socket };
+    io.emit('user connected', nickname);
+    const activeUsers = Object.values(connectedUsers).map(user => user.nickname);
+    io.emit('active users', activeUsers);
+  });
 
-    socket.on('chat message', (msg, recipient) => {
-        const nickname = connectedUsers[socket.id];
-        io.emit('chat message', { nickname, msg, recipient });
-    });
+  socket.on('chat message', (msg, recipient) => {
+    console.log('message: ' + msg);
+    const senderNickname = connectedUsers[socket.id].nickname;
+    if (recipient) {
+      const recipientSocket = Object.values(connectedUsers).find(
+        (user) => user.nickname === recipient
+      );
+      if (recipientSocket) {
+        recipientSocket.connection.emit('private message', {
+          nickname: senderNickname,
+          msg: msg,
+          recipient: recipient,
+        });
+        socket.emit('private message', {
+          nickname: senderNickname,
+          msg: msg,
+          recipient: recipient,
+        });
+      } else {
+        socket.emit('chat message', {
+          nickname: 'Server',
+          msg: `User "${recipient}" not found or not connected.`,
+        });
+      }
+    } else {
+      io.emit('chat message', { nickname: senderNickname, msg: msg });
+    }
+  });
 
-    socket.on('private message', ({ recipient, message }) => {
-        const nickname = connectedUsers[socket.id];
-        io.to(recipient).to(socket.id).emit('private message', { nickname, msg: message, recipient });
-    });
-
-    socket.on('disconnect', () => {
+ socket.on('disconnect', () => {
         const nickname = connectedUsers[socket.id];
         delete connectedUsers[socket.id];
         io.emit('user disconnected', nickname);
